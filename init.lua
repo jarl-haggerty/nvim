b32dfadee46938bed68f1a344cca3c37e89771ab
Jarl Haggerty <jarl@covetouscat.com>
2023-07-23 02:10:04 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.cmd("colorscheme desert")
+vim.opt.colorcolumn = "79"
+vim.opt.hlsearch = true
+vim.wo.number = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
