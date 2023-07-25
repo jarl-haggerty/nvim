8253ec0b89a9c8f45086c0309a56612d0ffe84d6
Jarl Haggerty <jarl@covetouscat.com>
2023-07-25 10:03:28 -0400

    Fix python tabs

--- init.lua
+++ init.lua
@@ -9,3 +9,14 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+    vim.opt_local.smartindent = true
+	end
+})
+
