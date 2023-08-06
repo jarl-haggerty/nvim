8139936bd3b95df0303babb5db04945ce6730047
Jarl Haggerty <jarl@covetouscat.com>
2023-08-06 00:17:51 -0400

    python change

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.cmd("colorscheme desert")
-vim.opt.colorcolumn = "79"
+vim.opt.colorcolumn = "119"
 vim.opt.hlsearch = true
 vim.wo.number = true
 vim.opt.tabstop = 2
@@ -17,6 +17,28 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.shiftwidth = 2
     vim.opt_local.expandtab = true
     vim.opt_local.smartindent = true
+
+    vim.lsp.start({
+      name = 'pyright',
+      cmd = {'pyright-langserver', '--stdio'},
+      root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
+    })
 	end
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {"*.cpp", "*.cc", "*.c", "*.h", "*.hpp"},
+	callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+    vim.opt_local.smartindent = true
+
+    vim.lsp.start({
+      name = 'clangd',
+      cmd = {'clangd'},
+      root_dir = vim.fs.dirname(vim.fs.find({'CMakeLists.txt'}, { upward = true })[1]),
+    })
+	end
+})
